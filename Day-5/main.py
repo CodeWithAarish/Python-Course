cb40067b4e2c66a96882860e16096901928c3c0d
CodeWithAarish <aarishm224@gmail.com>
2023-03-07 00:17:51 +0530

    Day-5 Completed

---
+++ Day-5/main.py
@@ -0,0 +1,12 @@
+'''
+Hey Aarish, Please don't removve this line
+Author: Aarish
+Course: 100DausOfCode
+'''
+print("Hey I am a good boy\nand this viewer is also a good boy/girl")
+
+# print("Hello World")
+# #This is a 'Single-Line Comment'
+# print("This is a print statement.")
+# print("Hello World !!!") #Printing Hello World
+
