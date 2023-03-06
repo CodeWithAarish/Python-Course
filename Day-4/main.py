842647f39f3bf99b809a0bed7221c4e4ceeda797
CodeWithAarish <aarishm224@gmail.com>
2023-03-07 00:15:37 +0530

    Day-4 Completed

---
+++ Day-4/main.py
@@ -0,0 +1,11 @@
+print("Hello World", 7)
+print(5)
+print("Bye")
+print(17*13)
+
+# Quick Quiz Code :-
+print('''Underneath my outside face
+There's a face that none can see.
+A little less smiley,
+A little less sure,
+But a whole lot more like me''')
