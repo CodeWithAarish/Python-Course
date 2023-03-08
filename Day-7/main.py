82e0b7a53fe2567211d7ddd76d57287f2b986bd6
CodeWithAarish <aarishm224@gmail.com>
2023-03-08 15:41:12 +0530

    Day-7 Completed

---
+++ Day-7/main.py
@@ -0,0 +1,27 @@
+# print(5+6)
+# print(15-6)
+# print(15*6)
+# print(15/6)
+# print(15//6)
+# print(5%3)
+# print(2**4)
+
+# Solution For The Exercise :-
+print("This is the solution of the problem.")
+
+a = 14
+b = 12
+ans1 = a + b
+print("After adding", a, "and", b, "we get", ans1)
+ans2 = a - b
+print("After subtracting", b, "from", a, "we get", ans2)
+ans3 = a * b
+print("After multiplying", a, "and", b, "we get", ans3)
+ans4 = a / b
+print("After dividing", a, "and", b, "we get", ans4)
+ans5 = a ** b
+print("After exponenting", a, "and", b, "we get", ans5)
+ans6 = a // b
+print("After floor division of", a, "and ", b, "we get", ans6)
+ans7 = a % b
+print("After modulus", a, "and", b, "we get", ans7)
