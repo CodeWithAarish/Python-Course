4bd4de45d7a52f3da737695825a5f8258f97848b
CodeWithAarish <aarishm224@gmail.com>
2023-03-14 23:23:44 +0530

    Day-13 Completed

---
+++ Day-13/main.py
@@ -0,0 +1,11 @@
+# String are immutabl in Python
+a = "!!!Aarish!! !!!!!!!!! Aarish"
+print(len(a))
+print(a)
+print(a.upper())
+print(a.lower())
+print(a.rstrip("!"))
+print(a.replace("Aarish", "John"))
+print(a.split(" "))
+blogHeading = "introduction tO jS"
+print(blogHeading.capitalize())
